c85fc73e09e40b5dd3fbc8099aba1307dcc3fe2c
obavanteshia <obavanteshia65@gmail.com>
2026-07-13 16:31:32 +0300

    fix

--- models/Payment.js
+++ models/Payment.js
@@ -9,7 +9,7 @@ const PaymentSchema = new mongoose.Schema({
   profileId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: false
   },
   amount: {
     type: Number,
